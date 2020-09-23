7fa6d19c9e06651de0e04f72dd7907878b6fec9c
Ed Bueler <elbueler@alaska.edu>
2020-09-23 13:58:08 -0800

    work on figures

--- doc/cartoon.py
+++ doc/cartoon.py
@@ -4,7 +4,12 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sys import exit
 
-def genbasicfig(timestep=False, icetext='', bedtext=''):
+plt.rcParams["font.family"] = "serif"
+plt.rcParams["mathtext.fontset"] = "cm"
+
+fsize = 20.0
+
+def genbasicfig(icetext='', bedtext=''):
     x = np.linspace(0.0,10.0,1001)
     b = 0.07*(x-3.0)**2 + 0.2*np.sin(2.0*x)
     plt.plot(x, b, '--k', lw=3.0)
@@ -12,41 +17,19 @@ def genbasicfig(timestep=False, icetext='', bedtext=''):
     h0 = 3.0
     L = 3.0
 
-    if timestep:
-        hm = np.maximum(0.0, 1.1*h0*(-0.2 + np.sqrt(np.maximum(0.0,1.0 - (x-4.5)**2/L**2))) )
-        sm = b + hm
-        plt.plot(x, sm, 'k:', lw=3.0)
-
     h = np.maximum(0.0, h0*(-0.2 + np.sqrt(np.maximum(0.0,1.0 - (x-5)**2/L**2))) )
     s = b + h
     plt.plot(x, s, 'k', lw=3.0)
 
-    if timestep:
-        plt.arrow(x[750],s[750]+1.0,x[700]-x[750],-0.55,lw=1.0,head_width=0.1,color='k',
-                  length_includes_head=True)
-        #plt.text(x[750],s[750]+1.0,r'$u_n$',fontsize=24.0,color='k')
-        plt.arrow(x[260],sm[260]+1.0,x[300]-x[260],-0.45,lw=1.0,head_width=0.1,color='k',
-                  length_includes_head=True)
-        plt.text(x[150],sm[250]+1.0,r'$u_{n-1}$',fontsize=24.0,color='k')
-    else:
-        #plt.arrow(x[600],b[600],0.0,h[600],lw=1.0,head_width=0.1,color='k',
-        #          length_includes_head=True)
-        #plt.arrow(x[600],s[600],0.0,-h[600],lw=1.0,head_width=0.1,color='k',
-        #          length_includes_head=True)
-        #plt.text(x[600]-0.4,b[600]+0.4*h[600],r'$u$',fontsize=24.0,color='k')
-        #plt.arrow(x[450],b[600]+0.3*h[600],-1.0,0.0,
-        #          lw=2.0,head_width=0.2,color='k',length_includes_head=True)
-        #plt.text(x[400],b[600]+0.45*h[600],r'$\mathbf{q}$',fontsize=24.0,color='k')
-        plt.text(x[600]-1.0,b[600]+0.4*h[600],icetext,fontsize=20.0,color='k')
-        plt.text(x[900]-0.4,b[900]-1.0,bedtext,fontsize=20.0,color='k')
+    plt.text(x[600]-1.0,b[600]+0.4*h[600],icetext,fontsize=fsize,color='k')
+    plt.text(x[900]-0.4,b[900]-1.0,bedtext,fontsize=fsize,color='k')
 
     plt.axis([0.0,10.0,-0.5,4.5])
     plt.axis('off')
     return x, s, b
 
 def drawclimate(x,s,mycolor,climatetext=''):
-    #plt.text(x[0]-0.5,s.max()+0.1,r'$f$',fontsize=24.0)
-    plt.text(x[0]-0.2,s.max()-0.7,climatetext,fontsize=20.0)
+    plt.text(x[0]-0.2,s.max()-0.7,climatetext,fontsize=fsize)
     for j in range(10):
         xarr = x[50+100*j]
         if j>0:
@@ -63,14 +46,37 @@ def figsave(name):
     else:
         plt.savefig(name,bbox_inches='tight')
 
-# basic figure:  u, q, f
+# basic figure
 plt.figure(figsize=(10,4))
-x, s, _ = genbasicfig(timestep=False,icetext='ice',bedtext='bedrock')
+x, s, b = genbasicfig(icetext='ice',bedtext='bedrock')
 drawclimate(x,s,'k',climatetext='surface mass balance')
 figsave('cartoon.pdf')
 
-## time step figure:  u_n-1, u_n
-#plt.figure(figsize=(10,4))
-#genbasicfig(timestep=True)
-#figsave('cartoon-dt.pdf')
+# domain notation figure
+plt.figure(figsize=(10,5.5))
+genbasicfig(icetext=r'$\Omega^t$',bedtext='')
+drawclimate(x,s,'k',climatetext='')
+# mark top surface
+plt.annotate(r'$\overline{\partial}\Omega^t$',fontsize=fsize,
+             xy=(x[300], s[300]),
+             xytext=(x[300]-1.5,s[300]+0.5),
+             arrowprops=dict(facecolor='black', width=0.5, headwidth=5.0, shrink=0.1))
+# mark bottom surface
+# BIZARRE HACK NEEDED BECAUSE \underline{} THROWS ERROR
+#plt.annotate(r"$\underline{\partial}\Omega^t$",fontsize=fsize,
+plt.annotate(r"$\partial\Omega^t$",fontsize=fsize,
+             xy=(x[700], b[700]),
+             xytext=(x[700]+1.1,b[700]-1.0),
+             arrowprops=dict(facecolor='black', width=0.5, headwidth=5.0, shrink=0.1))
+plt.text(x[700]+1.08,b[700]-1.02,r"$\_$",fontsize=24.0)  # HACK UNDERLINE
+# show \pi \Omega^t
+ypi = min(b) - 0.5
+plt.plot([min(x[s>b]),max(x[s>b])],[ypi,ypi],color='k',lw=1.0)
+plt.text(3.5,ypi-0.5,r'$\pi\Omega^t$',fontsize=fsize)
+# show R
+yR = min(b) - 1.7
+plt.plot([min(x),max(x)],[yR,yR],color='k',lw=1.0)
+plt.text(2.0,yR-0.5,r'$R$',fontsize=fsize)
+plt.axis([0.0,10.0,yR-0.8,4.5])
+figsave('domainnotation.pdf')
 
