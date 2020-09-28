cc8675f7f662f44213b7ccef63a96010ebe3c324
Ed Bueler <elbueler@alaska.edu>
2020-09-27 21:49:24 -0800

    fiddle with comments

--- py/stokes2D.py
+++ py/stokes2D.py
@@ -1,12 +1,15 @@
 #!/usr/bin/env python3
 
+# SHOWS EXTRUDED ORDERING:
+# ./stokes2D.py -s_ksp_view_mat draw -draw_pause -1 -draw_size 1000,1000 -nintervals 10 -layers 4
+
 from firedrake import *
 
 import argparse
 parser = argparse.ArgumentParser(description=
 '''
 Generate 2D mesh from Halfar (1981) used extrusion of an equally-spaced
-interval mesh.
+interval mesh.  Solve Laplace equation for reference domain scheme for SMB.
 ''')
 parser.add_argument('-H0', type=float, default=5000.0, metavar='X',
                     help='center height in m of ice sheet (default=5000)')
@@ -32,8 +35,7 @@ n = 3.0
 A = 1.0e-16/secpera
 Gamma = 2.0 * A * (rho * g)**3.0 / 5.0   # see Bueler et al (2005)
 
-# Halfar solution
-# for t0, see equation (9) in Bueler et al (2005); result is 422.45 a
+# Halfar (1981) solution
 alpha = 1.0/11.0
 t0 = (alpha/Gamma) * (7.0/4.0)**3.0 * (args.R0**4.0 / args.H0**7.0)
 def halfar2d(x):
@@ -43,8 +45,9 @@ def halfar2d(x):
     inside = max_value(0.0, 1.0 - pow(absx / args.R0,inpow))
     return args.H0 * pow(inside,outpow)
 
-dxelem = 2.0*args.L/args.nintervals
-dyrefelem = args.Href/args.layers
+# report on generated geometry and mesh
+dxelem = 2.0 * args.L / args.nintervals
+dyrefelem = args.Href / args.layers
 PETSc.Sys.Print('generating 2D Halfar geometry on interval [%.2f,%.2f] km,'
                 % (-args.L/1000.0,args.L/1000.0))
 PETSc.Sys.Print('    with H0=%.2f m and R0=%.2f km, at t0=%.5f a,'
@@ -69,10 +72,10 @@ mesh.coordinates.assign(f)
 # space in which to solve Laplace equation (nabla^2 u = 0) with Dirichlet bcs
 V = FunctionSpace(mesh, "CG", 1)
 
-# simulate surface kinematical condition value for top
+# simulate surface kinematical condition value for top; FIXME actual!
 dt = secpera  # 1 year time steps
 a = Constant(0.0)
-smbref = conditional(Hinitial > args.Href, dt*a, dt*a - args.Href)  # FIXME actual surface kinematical
+smbref = conditional(Hinitial > args.Href, dt*a, dt*a - args.Href)  # FIXME
 bcs = [DirichletBC(V, smbref, 'top'),
        DirichletBC(V, Constant(0.0), 'bottom')]
 
