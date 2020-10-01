d21ca337e0117cd83b5f397365ec47fa32d7a2d4
Ed Bueler <elbueler@alaska.edu>
2020-09-30 18:12:57 -0800

    a code put into firedrake slack which throws same error

---
+++ py/ebcsmbtest.py
@@ -0,0 +1,17 @@
+from firedrake import *
+
+mesh = UnitSquareMesh(10,10)
+x,y = SpatialCoordinate(mesh)
+
+Vu = VectorFunctionSpace(mesh, 'CG', degree=2)
+Vp = FunctionSpace(mesh, 'DG', degree=0)
+Vc = FunctionSpace(mesh, 'CG', degree=1)
+Z = Vu * Vp * Vc
+
+upc = Function(Z)
+u,p,c = split(upc)
+v,q,e = TestFunctions(Z)
+
+smb = - u[0] * y.dx(0) + u[1]
+EquationBC(smb * e * ds(1) == 0, upc, 1)
+
