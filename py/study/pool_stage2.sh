e8856b0da3d19fcc406bfaa85eb049402d2249bc
Ed Bueler <elbueler@alaska.edu>
2020-11-09 13:59:42 -0900

    Optimality for stage 2.  (It may make sense to eliminate stage 2.)

---
+++ py/study/pool_stage2.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+set -e
+set +x
+
+# run optimality study with 8 processes for stage 2
+# meshes: 4x4x4, 8x8x8, 16x16x16, 32x32x32, 64x64x64
+
+NP=8
+
+# inputs: $1 = level number
+#         $2 = mesh specification
+function runcase() {
+    CMD="mpiexec --bind-to hwthread --map-by core -n $NP ../pool.py -stage 2 $2 -log_view"
+    echo
+    echo $CMD
+    TNAME=stage2_lev$1.txt
+    rm -f $TNAME
+    $CMD &> $TNAME
+    head -n 6 $TNAME            # first 6 lines are before -log_view
+    grep "PCSetUp " $TNAME
+    grep "KSPSolve " $TNAME
+    grep "Time (sec):" $TNAME
+}
+
+runcase 0 "-mx 4 -my 4 -refine 1 -aggressive"
+runcase 1 "-mx 8 -my 8 -mz 2 -refine 1 -aggressive"
+runcase 2 "-mx 16 -my 16 -refine 2 -aggressive"
+runcase 3 "-mx 32 -my 32 -mz 2 -refine 2 -aggressive"
+runcase 4 "-mx 64 -my 64 -refine 3 -aggressive"
+
